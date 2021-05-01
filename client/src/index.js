858da2f17217d04142fe1ce3b88116ef6ba3c3a1
sapirdeu <sapiroosh.d@gmail.com>
2021-05-01 22:09:45 +0300

    setting redux up

--- client/src/index.js
+++ client/src/index.js
@@ -6,26 +6,21 @@ import './Resources/css/styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
-// import {Provider} from 'react-redux';
-// import {createStore, applyMiddleware} from 'redux';
-// import promiseMiddleware from 'redux-promise';
-// import ReduxThunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import promiseMiddleware from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
 
-// import Reducer from './redux/reducers/Reducer'
+import Reducer from './redux/reducers/Reducer'
 
-// const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 ReactDOM.render(
-  // <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
+  <Provider store={createStoreWithMiddleware(Reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <BrowserRouter>
       <Routes />
     </BrowserRouter>
-  // </Provider>
+  </Provider>
   ,
   document.getElementById('root')
 );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
---
+++ client/src/redux/actions/user_actions.js
@@ -0,0 +1,152 @@
+import axios from 'axios'
+// import {
+//         LOGIN_USER, 
+//         REGISTER_USER, 
+//         AUTH_USER, 
+//         LOGOUT_USER,
+//         ADD_TO_CART_USER,
+//         GET_CART_ITEMS_USER,
+//         REMOVE_CART_ITEMS_USER,
+//         ON_SUCCESS_BUY_USER,
+//         UPDATE_DATA_USER,
+//         CLEAR_UPDATE_USER_DATA
+// } from './Types'
+import {USER_SERVER, PRODUCT_SERVER} from '../../components/utils/Misc'
+
+// function registerUser(dataToSubmit){
+//     const request = 
+//         axios.post(`${USER_SERVER}/registers`, dataToSubmit)
+//         .then(response => response.data);
+    
+//     return {
+//         type: REGISTER_USER, 
+//         payload: request
+//     }
+// }
+
+// function loginUser(dataToSubmit){
+//     const request = 
+//         axios.post(`${USER_SERVER}/login`, dataToSubmit)
+//         .then(response => response.data);
+    
+//     return {
+//         type: LOGIN_USER, 
+//         payload: request
+//     }
+// }
+
+// function auth(){
+//     const request = 
+//         axios.get(`${USER_SERVER}/auth`)
+//         .then(response => response.data);
+    
+//     return {
+//         type: AUTH_USER, 
+//         payload: request
+//     }
+// }
+
+// function logoutUser(){
+//     const request = 
+//         axios.get(`${USER_SERVER}/logout`)
+//         .then(response => response.data);
+    
+//     return {
+//         type: LOGOUT_USER, 
+//         payload: request
+//     }
+// }
+
+// function addToCart(id){
+//     const request = 
+//     axios.post(`${USER_SERVER}/addToCart?productId=${id}`)
+//     .then(response => response.data);
+
+//     return {
+//         type: ADD_TO_CART_USER, 
+//         payload: request
+//     }
+// }
+
+// function getCartItems(cartItems, userCart){
+//     const request = 
+//         axios.get(`${PRODUCT_SERVER}/articles_by_id?id=${cartItems}&type=array`)
+//         .then(response => {
+//             userCart.forEach(item=>{
+//                 response.data.forEach((k,i)=>{
+//                     if(item.id === k._id){
+//                         response.data[i].quantity = item.quantity;
+//                     }
+//                 })
+//             })
+//             return response.data;
+//         });
+    
+//     return {
+//         type: GET_CART_ITEMS_USER, 
+//         payload: request
+//     }
+// }
+
+// function removeCartItem(id){
+//     const request = 
+//         axios.get(`${USER_SERVER}/removeFromCart?_id=${id}`)
+//         .then(response => {
+//             response.data.cart.forEach(item=>{
+//                 response.data.cartDetail.forEach((currItem,i)=>{
+//                     if(item.id === currItem._id){
+//                         response.data.cartDetail[i].quantity = item.quantity;
+//                     }
+//                 })
+//             })
+//             return response.data;
+//         });
+    
+//     return {
+//         type: REMOVE_CART_ITEMS_USER, 
+//         payload: request
+//     }
+// }
+
+// function onSuccessBuy(data){
+//     const request = 
+//     axios.post(`${USER_SERVER}/successBuy`, data)
+//     .then(response => response.data);
+
+//     return {
+//         type: ON_SUCCESS_BUY_USER, 
+//         payload: request
+//     }
+// }
+
+// function updateUserData(dataToSubmit){
+//     const request = 
+//     axios.post(`${USER_SERVER}/update_profile`, dataToSubmit)
+//     .then(response => response.data);
+
+//     return {
+//         type: UPDATE_DATA_USER, 
+//         payload: request
+//     }
+// }
+
+// function clearUpdateUser(){
+//     return {
+//         type: CLEAR_UPDATE_USER_DATA, 
+//         payload: ''
+//     }
+// }
+
+// export {
+//     loginUser, 
+//     registerUser, 
+//     auth, 
+//     logoutUser, 
+//     addToCart, 
+//     getCartItems,
+//     removeCartItem,
+//     onSuccessBuy,
+//     updateUserData,
+//     clearUpdateUser
+// }
+
