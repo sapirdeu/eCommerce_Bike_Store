dada93de8948ae6f000ca01e3904f6dc21f8dd96
Almog Lev <almoglev3@gmail.com>
2021-05-07 00:34:27 +0300

    start ml research! reading dataset from csv file

---
+++ ml-research-bikes/main.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+
+CSV_PATH = "C:/Users\Almog/Desktop/final-project/eCommerce_Bike_Store/ml-research-bikes/bike_sharing_daily.csv"
+
+def prepareData(csvPath):
+    ######################################
+    ########## DATA PREPARATION ##########
+    ######################################
+
+    df = pd.read_csv(csvPath)
+    # Converting the dteday column which is of type object to datetime64
+    df['dteday'] = df['dteday'].astype('datetime64')
+    # As the instant columns in just the index removing them won't effect
+    df.drop(columns=['instant'], inplace=True)
+    df.drop(columns=['atemp'], inplace=True)
+    df.drop(columns=['mnth'], inplace=True)
+    df.drop(columns=['hum'], inplace=True)
+    df.drop(columns=['dteday'], inplace=True)
+
+    #
+    x = df.iloc[:, :-3].values
+    y = df.iloc[:, -3:].values
+
+    
+
+
+if __name__ == '__main__':
+    # prepare data
+    prepareData(CSV_PATH)
+    print("\n\nTHE END\n\n")
