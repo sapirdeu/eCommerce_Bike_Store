4d24af68f990cc857e7558e99d3bbaa57b4c5061
Almog Lev <almoglev3@gmail.com>
2021-05-13 15:20:49 +0300

    start the anomaly detector

--- ml-research-bikes/main.py
+++ ml-research-bikes/main.py
@@ -1,44 +1,46 @@
-import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-
-
+from sklearn.cluster import KMeans
+from sklearn.ensemble import IsolationForest, RandomForestRegressor
+from sklearn.decomposition import PCA
+from sklearn.svm import SVR
 
 warnings.filterwarnings('ignore')
 
-CSV_PATH = "./bike_sharing_daily.csv"
+CSV_PATH = "./bike_sharing.csv"
+CSV_ANOM_PATH = "./anomalies.csv"
+CSV_NO_ANOMALIES_PATH = "./bike_sharing_no_anomalies.csv"
 
-#- instant: record index
-#- dteday : date
-#- season : season (1:springer, 2:summer, 3:fall, 4:winter)
-#- yr : year (0: 2011, 1:2012)
-#- mnth : month ( 1 to 12)
-#- holiday : weather day is holiday or not (extracted from http://dchr.dc.gov/page/holiday-schedule)
-#- weekday : day of the week
-#- workingday : if day is neither weekend nor holiday is 1, otherwise is 0.
-#+ weathersit :
+# - instant: record index
+# - dteday : date
+# - season : season (1:springer, 2:summer, 3:fall, 4:winter)
+# - yr : year (0: 2011, 1:2012)
+# - mnth : month ( 1 to 12)
+# - holiday : weather day is holiday or not (extracted from http://dchr.dc.gov/page/holiday-schedule)
+# - weekday : day of the week
+# - workingday : if day is neither weekend nor holiday is 1, otherwise is 0.
+# + weathersit :
 #    - 1: Clear, Few clouds, Partly cloudy, Partly cloudy
 #    - 2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
 #    - 3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
 #    - 4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
-#- temp : Normalized temperature in Celsius. The values are divided to 41 (max)
-#- atemp: Normalized feeling temperature in Celsius. The values are divided to 50 (max)
-#- hum: Normalized humidity. The values are divided to 100 (max)
-#- windspeed: Normalized wind speed. The values are divided to 67 (max)
-#- casual: count of casual users
-#- registered: count of registered users
-#- cnt: count of total rental bikes including both casual and registered
+# - temp : Normalized temperature in Celsius. The values are divided to 41 (max)
+# - atemp: Normalized feeling temperature in Celsius. The values are divided to 50 (max)
+# - hum: Normalized humidity. The values are divided to 100 (max)
+# - windspeed: Normalized wind speed. The values are divided to 67 (max)
+# - casual: count of casual users
+# - registered: count of registered users
+# - cnt: count of total rental bikes including both casual and registered
 
 
+# fixing the data type of the dataset
 def fixingDatatypes(df):
     # some data types need to be changed from numerical to categorical or bool
     # otherwise the prediction model can interpret wrongly
@@ -52,18 +54,7 @@ def fixingDatatypes(df):
     df['weathersit'] = df.weathersit.astype('category')
     # confirming the converted datatype
     print(df.dtypes)
-
     return df
-
-
-
-# showing the heatmap to check linear correlation between features
-def plotHeatmap(df):
-    # check the correlation of the features TODO: make a heatmap function
-    heatmapFeatures = df[['cnt', 'season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday',
-                   'weathersit', 'temp', 'atemp', 'hum', 'windspeed']]
-    sns.heatmap(abs(heatmapFeatures.corr()), square=True, annot=True, cmap="Reds")
-    plt.show()  ##### 5.5.2021 heatmap graph
 
 
 # pre-processing and analyzing the data
@@ -78,7 +69,7 @@ def prepareData(csvPath):
     print(df.info())
 
     # fix datatypes
-    df=fixingDatatypes(df)
+    df = fixingDatatypes(df)
 
     # getting some interesting info about count
     print(df['cnt'].describe())
@@ -99,18 +90,26 @@ def prepareData(csvPath):
     return x, y
 
 
+# showing the heatmap to check linear correlation between features
+def plotHeatmap(df):
+    # check the correlation of the features
+    heatmapFeatures = df[['cnt', 'season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday',
+                          'weathersit', 'temp', 'atemp', 'hum', 'windspeed']]
+    sns.heatmap(abs(heatmapFeatures.corr()), square=True, annot=True, cmap="Reds")
+    plt.show()  #heatmap graph
+
+
 # receives the model with the data and executes it. returns the loss and the predictions
 def executeModel(model, x_train, x_test, y_train, y_test):
     model.fit(x_train, y_train[:, 2])
     y_hat = model.predict(x_test)
-    print(np.round(y_hat))
-    print(y_test[:, 2])
-    modelR2 = r2_score(y_test[:, 2], y_hat)
+    #print(np.round(y_hat))
+    #print(y_test[:, 2])
     # Mean Square Error- the sum of squared distances between the target variable and predicted values
     # We chose MSE as our loss function because it is outlier sensitive: sigma((y_hat-y_test)^2)
-    modelMSE = mean_squared_error(y_hat,y_test[:, 2])
+    modelMSE = mean_squared_error(y_hat, y_test[:, 2])
     # normalize MSE (divided by the ||y_hat||^2)
-    modelMSE = modelMSE/(mean_squared_error(y_hat,np.zeros(len(y_hat))))
+    modelMSE = modelMSE / (mean_squared_error(y_hat, np.zeros(len(y_hat))))
     return modelMSE, y_hat
 
 
@@ -156,9 +155,125 @@ def predictCount(file_path):
     executeAllModels(x, y)
 
 
+# detecting anomalies in the dataset using PCA, K-Means and Isolation Forest
+def detectAnomalies(file_path):
+    # reading the data and fixing data types
+    df = pd.read_csv(file_path)
+    df = fixingDatatypes(df)
+
+    # subset of the dataset for the anomaly detection test without dtday, casual, registered, instant and atemp
+    # removing features that are not needed- ambiguous or add no importance to the analysis
+    reduced_df = df[['cnt', 'season', 'yr', 'mnth', 'holiday', 'weekday', 'workingday',
+                     'weathersit', 'temp', 'hum', 'windspeed']]
+
+    # Z-score- standardize features by removing the mean and scaling to unit variance: z = (x - u) / s
+    reduced_df = zScoreNormalize(reduced_df)  # graph before PCA
+
+    # performing PCA - reduce to 2 important features
+    pca = PCA(n_components=2)
+    reduced_df = pca.fit_transform(reduced_df)  # graph after PCA but before min_max normalization
+    # standardize these 2 new features, now we can perform K-Means and Isolation Forest
+    reduced_df = zScoreNormalize(reduced_df)  # graph after PCA
+
+    # perform K-Means
+    K_Means(df, reduced_df)
+
+    # perform Isolation Forest
+    Isolation_forest(df, reduced_df)
+
+    # save anomalies in file
+    saveAnomalies(df)
+
+
+# Z-score- standardize features by removing the mean and scaling to unit variance: z = (x - u) / s
+def zScoreNormalize(reduced_df):
+    Scaler = StandardScaler()
+    np_scaled = Scaler.fit_transform(reduced_df)  # plot info, put breakpoint here to show PCA
+    return pd.DataFrame(np_scaled)
+
+
+# performing K-Means end to end: elbow function, choosing k, clusters, anomalies
+def K_Means(df, reduced_df):
+    # elbow method to choose K
+    n_cluster = range(1, 12)
+    kmeans = [KMeans(n_clusters=i).fit(reduced_df) for i in n_cluster]
+    scores = [kmeans[i].score(reduced_df) for i in range(len(kmeans))]
+    fig, ax = plt.subplots()
+    ax.plot(n_cluster, scores)
+    plt.show()
+
+    # chosen K is 3, hence index 2 and plot the sticks graph
+    cluster_index = 2
+    df['cluster'] = kmeans[cluster_index].predict(reduced_df)
+    df['PCA1'] = reduced_df[0]
+    df['PCA2'] = reduced_df[1]
+    plt.yticks(df['cluster'].value_counts())
+    df['cluster'].value_counts().plot.bar()
+    plt.show()
+
+    # K-Means result plot the different clusters with the 2 main features
+    plotAnomalyGraph(df, "cluster")
+
+    outliers_fraction = 0.05
+    # get the distance between each point and its nearest centroid. The biggest distances are considered as anomalies.
+    distance = getDistanceByPoint(reduced_df, kmeans[cluster_index])
+    number_of_outliers = int(outliers_fraction * len(distance))
+
+    # return the first number_of_outliers rows ordered by columns in descending order.
+    threshold = distance.nlargest(number_of_outliers).min()
+
+    # create column for the anomaly result of K-Means (0:normal, 1:anomaly)
+    df['Anom-KMeans'] = (distance >= threshold).astype(int)
+    plotAnomalyGraph(df, 'Anom-KMeans')  # K-Means graph clusters (divided to anomalies)
+
+
+# returns the distance of each example from the center of it's cluster
+def getDistanceByPoint(data, model):
+    distance = pd.Series()
+    for i in range(0, len(data)):
+        Xa = np.array(data.loc[i])
+        Xb = model.cluster_centers_[model.labels_[i] - 1]
+        distance.at[i] = np.linalg.norm(Xa - Xb)
+    return distance
+
+
+# performing Isolation Forest
+def Isolation_forest(df, reduced_df):
+    outliers_fraction = 0.05
+    model = IsolationForest(contamination=outliers_fraction)
+    model.fit(reduced_df)
+    # create column for the anomaly result of Isolation Forest (0:normal, 1:anomaly)
+    df['Anom-Isol'] = pd.Series(model.predict(reduced_df))
+    df['Anom-Isol'] = df['Anom-Isol'].map({1: 0, -1: 1})
+    plotAnomalyGraph(df, 'Anom-Isol')  # isolation forest graph (divided to anomalies)
+
+
+# plot the graph for the anomalies
+def plotAnomalyGraph(df, hue_name):
+    plt.subplots(figsize=(7, 7))
+    sns.scatterplot(df['PCA1'], df['PCA2'], hue=df[hue_name],
+                    data=df, style=df["cluster"])
+    plt.show()
+
+
+# save anomalies in csv file and show results
+def saveAnomalies(df):
+    an = pd.DataFrame(df)
+    an.to_csv(CSV_ANOM_PATH, index=False, header=True)
+
+    # final results
+    print(df['Anom-KMeans'].value_counts())
+    print(df['Anom-Isol'].value_counts())
+
+
 if __name__ == '__main__':
     # predict count before anomaly-detector.
-    predictCount(CSV_PATH)
+    #predictCount(CSV_PATH)
+
+    # detect anomalies.
+    detectAnomalies(CSV_PATH)
+
+    # predict count after anomalies are removed from the dataset -> loss is lower -> better accuracy.
+    #predictCount(CSV_NO_ANOMALIES_PATH)
 
     print("\n\nTHE END\n\n")
-
